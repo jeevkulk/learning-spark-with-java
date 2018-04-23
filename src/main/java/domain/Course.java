04445772eed144c346076036522dad80e9783edd
jeevkulk <jeevanakulkarni@gmail.com>
2018-04-23 11:20:24 +0530

    Added filter, map and flatMap transformations

--- src/main/java/domain/Course.java
+++ src/main/java/domain/Course.java
@@ -6,10 +6,18 @@ public class Course implements Serializable {
 
     private String code;
     private String name;
+    private boolean elective;
+    private int semester;
+    private String specialization;
+    private int credits;
 
-    public Course(String code, String name) {
+    public Course(String code, String name, boolean elective, int semester, String specialization, int credits) {
         this.code = code;
         this.name = name;
+        this.elective = elective;
+        this.semester = semester;
+        this.specialization = specialization;
+        this.credits = credits;
     }
 
     public String getCode() {
@@ -26,5 +34,37 @@ public class Course implements Serializable {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public boolean isElective() {
+        return elective;
+    }
+
+    public void setElective(boolean elective) {
+        this.elective = elective;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public String getSpecialization() {
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    public void setCredits(int credits) {
+        this.credits = credits;
     }
 }
--- src/test/java/sparkrdd/transformation/NarrowTransformationsTest.java
+++ src/test/java/sparkrdd/transformation/NarrowTransformationsTest.java
@@ -20,49 +20,61 @@ public class NarrowTransformationsTest {
     @Before
     public void setup() {
         courses = Arrays.asList(
-                new Course ("CE616", "Structural Dynamics"),
-                new Course ("CE623", "Advanced Solid Mechanics"),
-                new Course ("CE627", "Structural Design Lab"),
-                new Course ("CE694", "Seminar"),
-                new Course ("HS791", "Communication Skills"),
-                new Course ("CE792", "Communication Skills"),
-                new Course ("CE797", "I Stage Dissertation"),
-                new Course ("CE798", "II Stage Dissertation"),
-                new Course ("CE448", "Prestressed Concrete"),
-                new Course ("CE482", "Construction Management"),
-                new Course ("CE602", "Design of Offshore Structures"),
-                new Course ("CE603", "Numerical Methods"),
-                new Course ("CE605", "Applied Statistics"),
-                new Course ("CE610", "Introduction to Earthquake Engineering"),
-                new Course ("CE611", "Advanced Structural Mechanics"),
-                new Course ("CE615", "Structural Optimisation"),
-                new Course ("CE617", "Plates and Shells"),
-                new Course ("CE619", "Structural Stability"),
-                new Course ("CE620", "Finite Element Method"),
-                new Course ("CE621", "Plastic Analysis"),
-                new Course ("CE624", "Nonlinear Analysis"),
-                new Course ("CE625", "Analysis of Offshore Structures"),
-                new Course ("CE629", "Elastic Waves in Solids"),
-                new Course ("CE633", "Soil Structure Interaction"),
-                new Course ("CE639", "Green Building Design"),
-                new Course ("CE640", "Foundation Engineering"),
-                new Course ("CE647", "Soil Dynamics and Machine Foundations"),
-                new Course ("CE651", "Bridge Engineering"),
-                new Course ("CE653", "Structural Reliability and Risk Analysis"),
-                new Course ("CE679", "Advanced Mechanics of Reinforced Concrete"),
-                new Course ("CE684", "Advanced Geotechnical Earthquake Engineering"),
-                new Course ("CE713", "Advanced Concrete Technology"),
-                new Course ("CE719", "Construction Contracts"),
-                new Course ("CE720", "Non-destructive Testing of Materials"),
-                new Course ("CE727", "Construction Materials"),
-                new Course ("CE743", "Condition Assessment and Rehabilitation of Structures"),
-                new Course ("CE771", "Optimization in Civil Engineering")
+                new Course ("CE616", "Structural Dynamics",false,1,"CE4",6),
+                new Course ("CE623", "Advanced Solid Mechanics",false,1,"CE4",6),
+                new Course ("CE627", "Structural Design Lab",false,1,"CE4",4),
+                new Course ("CE694", "Seminar", false,1,"CE4",4),
+                new Course ("HS791", "Communication Skills",false,2,"CE4",2),
+                new Course ("CE792", "Communication Skills",false,2,"CE4",4),
+                new Course ("CE797", "I Stage Dissertation",false,3,"CE4",48),
+                new Course ("CE798", "II Stage Dissertation",false,4,"CE4",42),
+                new Course ("CE448", "Prestressed Concrete",true,0,"CE4",6),
+                new Course ("CE482", "Construction Management",true,0,"CE4",6),
+                new Course ("CE602", "Design of Offshore Structures",true,0,"CE4",6),
+                new Course ("CE603", "Numerical Methods",true,0,"CE4",6),
+                new Course ("CE605", "Applied Statistics",true,0,"CE4",6),
+                new Course ("CE610", "Introduction to Earthquake Engineering",true,0,"CE4",6),
+                new Course ("CE611", "Advanced Structural Mechanics",true,0,"CE4",6),
+                new Course ("CE615", "Structural Optimisation",true,0,"CE4",6),
+                new Course ("CE617", "Plates and Shells",true,0,"CE4",6),
+                new Course ("CE619", "Structural Stability",true,0,"CE4",6),
+                new Course ("CE620", "Finite Element Method",true,0,"CE4",6),
+                new Course ("CE621", "Plastic Analysis",true,0,"CE4",6),
+                new Course ("CE624", "Nonlinear Analysis",true,0,"CE4",6),
+                new Course ("CE625", "Analysis of Offshore Structures",true,0,"CE4",6),
+                new Course ("CE629", "Elastic Waves in Solids",true,0,"CE4",6),
+                new Course ("CE633", "Soil Structure Interaction",true,0,"CE4",6),
+                new Course ("CE639", "Green Building Design",true,0,"CE4",6),
+                new Course ("CE640", "Foundation Engineering",true,0,"CE4",6),
+                new Course ("CE647", "Soil Dynamics and Machine Foundations",true,0,"CE4",6),
+                new Course ("CE651", "Bridge Engineering",true,0,"CE4",6),
+                new Course ("CE653", "Structural Reliability and Risk Analysis",true,0,"CE4",6),
+                new Course ("CE679", "Advanced Mechanics of Reinforced Concrete",true,0,"CE4",6),
+                new Course ("CE684", "Advanced Geotechnical Earthquake Engineering",true,0,"CE4",6),
+                new Course ("CE713", "Advanced Concrete Technology",true,0,"CE4",6),
+                new Course ("CE719", "Construction Contracts",true,0,"CE4",6),
+                new Course ("CE720", "Non-destructive Testing of Materials",true,0,"CE4",6),
+                new Course ("CE727", "Construction Materials",true,0,"CE4",6),
+                new Course ("CE743", "Condition Assessment and Rehabilitation of Structures",true,0,"CE4",6),
+                new Course ("CE771", "Optimization in Civil Engineering",true,0,"CE4",6)
         );
+    }
+
+    @Test
+    public void testFilterOutElectiveCourses() {
+        JavaRDD<Course> rdd = narrowTransformations.filterOutElectiveCourses(courses);
+        Assert.assertTrue(rdd.count() == 8L);
     }
 
     @Test
     public void testMapToCourseName() {
         JavaRDD<String> rdd = narrowTransformations.mapToCourseName(courses);
         Assert.assertTrue(rdd.first() instanceof String);
+    }
+
+    @Test
+    public void testGetAllStrings() {
+        JavaRDD<String> rdd = narrowTransformations.getAllStrings(courses);
+        Assert.assertTrue(rdd.count() == 111L);
     }
 }
