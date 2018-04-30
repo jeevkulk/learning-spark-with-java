51ebff05ee41252c506550b06c92b688a5120625
jeevkulk <jeevanakulkarni@gmail.com>
2018-04-30 23:34:13 +0530

    Examples of Narrow RDD Transformations

--- src/main/java/sparkrdd/transformation/NarrowTransformations.java
+++ src/main/java/sparkrdd/transformation/NarrowTransformations.java
@@ -6,7 +6,10 @@ import org.apache.log4j.Logger;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
+import org.apache.spark.api.java.function.Function2;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
@@ -46,7 +49,7 @@ public enum NarrowTransformations {
      * @param courses
      * @return
      */
-    public JavaRDD<String> mapToCourseName(List<Course> courses) {
+    public JavaRDD<String> getAllCourseNamesUsingMap(List<Course> courses) {
         JavaSparkContext context = getJavaSparkContext();
         JavaRDD<Course> rdd = context.parallelize(courses);
         return rdd.map(course -> course.getName());
@@ -57,11 +60,91 @@ public enum NarrowTransformations {
      * @param courses
      * @return
      */
-    public JavaRDD<String> getAllStrings(List<Course> courses) {
+    public JavaRDD<String> getAllCourseCodesUsingFlatMap(List<Course> courses) {
         JavaSparkContext context = getJavaSparkContext();
         JavaRDD<Course> rdd = context.parallelize(courses);
         return rdd.flatMap(course -> {
             return Arrays.asList(course.getCode(), course.getCode(), course.getCode()).iterator();
         });
+    }
+
+    /**
+     * Gets all course names using mapPartitions - mapPartition runs only once per partition
+     * @param courses
+     * @return
+     */
+    public JavaRDD<String> getAllCourseNamesUsingMapPartition(List<Course> courses) {
+        JavaSparkContext context = getJavaSparkContext();
+        JavaRDD<Course> rdd = context.parallelize(courses);
+        /*FlatMapFunction<Iterator<Course>, String> iteratorFlatMapFunction = new FlatMapFunction<Iterator<Course>, String>() {
+            @Override
+            public Iterator<String> call(Iterator<Course> courseIterator) throws Exception {
+                List<String> list = new ArrayList<>();
+                while (courseIterator.hasNext()) {
+                    list.add(courseIterator.next().getName());
+                }
+                return list.iterator();
+            }
+        };
+        JavaRDD<String> rddStr = rdd.mapPartitions(iteratorFlatMapFunction);*/
+        JavaRDD<String> rddStr = rdd.mapPartitions(courseIterator -> {
+            List<String> list = new ArrayList<>();
+            while (courseIterator.hasNext()) {
+                list.add(courseIterator.next().getName());
+            }
+            return list.iterator();
+        });
+        return rddStr;
+    }
+
+    public JavaRDD<String> getAllCourseNamesUsingMapPartitionsWithIndex(List<Course> courses) {
+        JavaSparkContext context = getJavaSparkContext();
+        JavaRDD<Course> rdd = context.parallelize(courses);
+
+        /*Function2<Integer, Iterator<Course>, Iterator<String>> function2 = new Function2<Integer, Iterator<Course>, Iterator<String>>() {
+            @Override
+            public Iterator<String> call(Integer integer, Iterator<Course> courseIterator) throws Exception {
+                List<String> list = new ArrayList<>();
+                while (courseIterator.hasNext()) {
+                    list.add(courseIterator.next().getName());
+                }
+                return list.iterator();
+            }
+        };
+        JavaRDD<String> rddStr = rdd.mapPartitionsWithIndex(function2, true);*/
+
+        JavaRDD<String> rddStr = rdd.mapPartitionsWithIndex((integer, courseIterator) -> {
+            List<String> list = new ArrayList<>();
+            while (courseIterator.hasNext()) {
+                list.add(courseIterator.next().getName());
+            }
+            return list.iterator();
+        }, true);
+        return rddStr;
+    }
+
+    /**
+     * Returns sample RDD based parameters passed to sample method
+     * @param list
+     * @param fraction
+     * @return
+     */
+    public JavaRDD<Course> getSampleRDD(List<Course> list, boolean withReplacement, double fraction) {
+        JavaSparkContext context = getJavaSparkContext();
+        JavaRDD<Course> rdd = context.parallelize(list);
+        return rdd.sample(withReplacement, fraction, 1000L);
+    }
+
+    /**
+     * Union of two RDDs
+     * @param list1
+     * @param list2
+     * @return
+     */
+    public JavaRDD<Course> getUnionRDD(List<Course> list1, List<Course> list2) {
+        JavaSparkContext context = getJavaSparkContext();
+        JavaRDD<Course> rdd1 = context.parallelize(list1);
+        JavaRDD<Course> rdd2 = context.parallelize(list2);
+        return rdd1.union(rdd2);
     }
 }
